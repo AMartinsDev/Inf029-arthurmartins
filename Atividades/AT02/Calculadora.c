2cf0e7d10f1ce09fc4d1e92fe8119f1c01ec77f7
Arthur Martins <arthursilva0198@gmail.com>
2021-03-22 00:53:20 -0300

    Att Calc

--- Atividades/AT02/Calculadora.c
+++ Atividades/AT02/Calculadora.c
@@ -2,47 +2,14 @@
 #include<stdlib.h>
 #include<string.h>
 
-
-int somar (int x, int y)
-{
-    	int calc;
-
-    	calc = x + y;
-
-    	return calc;
-}
-
-int subtrair (int x, int y)
-{	
-	int calc;
-
-	calc = x - y;
-
-	return calc;
-}
-
-int multiplicar (int x, int y)
-{
-	int calc;
-
-	calc = x * y;
-	
-	return calc;
-}
-
-int dividir (int x, int y)
-{
-	int calc;
-	
-	calc = x/y;
-
-	return calc;
-
+int somar(int x, int y);
+int subtrair(int x, int y);
+int multiplicar(int x, int y);
+int dividir(int x, int y);
 
 int main()
 {
-
-    	int num1, num2, op, result;
+    int num1, num2, op, result;
 	
 	do
 	{
@@ -52,37 +19,37 @@ int main()
 		switch (op)
 		{
 			case 0:
-			printf("Obrigado por usar o programa:\n");
+			printf("Obrigado por usar o programa.\n");
 			break;
 
 			case 1:
-			printf("Digite o primeiro numero a ser somado:\n"); scanf("%d", &num1);
-    			fflush(stdin);
-    			printf("Digite o segundo numero a ser somado:\n"); scanf("%d", &num2);		
+			printf("Digite o primeiro numero a ser somado:\n");scanf("%d", &num1);
+    		fflush(stdin);
+    		printf("Digite o segundo numero a ser somado:\n"); scanf("%d", &num2);	
 			result = somar(num1,num2);
 			printf("O resultado da soma eh: %d \n", result);
 			break;
 
 			case 2:
 			printf("Digite o primeiro numero a ser subtraido:\n"); scanf("%d", &num1);
-    			fflush(stdin);
-    			printf("Digite o segundo numero a ser subtraido:\n"); scanf("%d", &num2);		
+    		fflush(stdin);
+    		printf("Digite o segundo numero a ser subtraido:\n"); scanf("%d", &num2);		
 			result = subtrair(num1,num2);
 			printf("O resultado da subtracao eh: %d \n", result);
 			break;
 			
 			case 3:
 			printf("Digite o primeiro numero a ser multiplicado:\n"); scanf("%d", &num1);
-    			fflush(stdin);
-    			printf("Digite o segundo numero a ser multiplicado:\n"); scanf("%d", &num2);		
-			result = multipicar(num1,num2);
+    		fflush(stdin);
+    		printf("Digite o segundo numero a ser multiplicado:\n"); scanf("%d", &num2);		
+			result = multiplicar(num1,num2);
 			printf("O resultado da multiplicacao eh: %d \n", result);
 			break;
 
 			case 4:
 			printf("Digite o primeiro numero a ser dividido:\n"); scanf("%d", &num1);
-    			fflush(stdin);
-    			printf("Digite o segundo numero a ser dividido:\n"); scanf("%d", &num2);		
+    		fflush(stdin);
+    		printf("Digite o segundo numero a ser dividido:\n"); scanf("%d", &num2);		
 			result = dividir(num1,num2);
 			printf("O resultado da divisao eh: %d \n", result);
 			break;
@@ -90,9 +57,44 @@ int main()
 			default:
 			printf("\nOpcao invalida\n");
 		}
+	}while(op!=0); 
 
-	}while(op>1);
-
-
-   	return 0;
+	return 0;
 }
+
+int somar(int x, int y)
+{
+    int calc;
+
+    calc = x + y;
+
+    return calc;
+}
+
+int subtrair(int x, int y)
+{	
+	int calc;
+
+	calc = x - y;
+
+	return calc;
+}
+
+int multiplicar(int x, int y)
+{
+	int calc;
+
+	calc = x * y;
+	
+	return calc;
+}
+
+int dividir(int x, int y)
+{
+	int calc;
+	
+	calc = x/y; 
+
+	return calc;
+}
+ 
