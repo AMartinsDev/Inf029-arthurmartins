9fab876940904b534c8353119e63ae08916dd754
Arthur Martins <arthursilva0198@gmail.com>
2021-03-30 02:01:48 -0300

    Iniciado desenvolvimento do projeto escola

---
+++ Atividades/ProjetoEscola/Projetoescola.c
@@ -0,0 +1,33 @@
+#include<stdio.h>
+#include<stdlib.h>
+#include<string.h>
+
+//Declaração de funções do menu de cadastro
+void Cadastrar();
+void CadastrarAluno();
+void CadastrarProfessor();
+void CadastrarDisciplina();
+
+//Declaração de funções do menu de listagem
+void Listar();
+void ListarAluno();
+void ListarProfessor();
+void ListarDisciplina();
+
+//Declaração de funções do menu de Inserção
+void Inserir();
+void InserirAluno();
+void InserirProfessor();
+
+
+
+int main(void)
+{
+    int opcao;
+
+    do{
+
+
+    }
+    
+}
